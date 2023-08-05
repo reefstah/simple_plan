17b3257423549d1f96feb99f64e73d4c0f15bcfc
bear <tiwari_shweta@outlook.com>
2023-08-05 20:57:59 +0200

    running tests in parallel

--- event_store/src/plannable_events_repository.rs
+++ event_store/src/plannable_events_repository.rs
@@ -6,15 +6,14 @@ use diesel::prelude::*;
 use diesel::SqliteConnection;
 use std::io::Error;
 use std::io::ErrorKind;
-
 struct PlannableEventsRepository {
     sql_connection: SqliteConnection,
 }
 
 impl PlannableEventsRepository {
-    fn initialize() -> Result<Self, std::io::Error> {
+    fn initialize(database_url: &str) -> Result<Self, std::io::Error> {
         Ok(Self {
-            sql_connection: establish_connection()?,
+            sql_connection: establish_connection(database_url)?,
         })
     }
     fn save(mut self, eventrow: Vec<PlannableEventRow>) -> Result<(), diesel::result::Error> {
@@ -28,20 +27,33 @@ impl PlannableEventsRepository {
             .execute(&mut self.sql_connection)
             .map(|size| ())
     }
-}
-fn establish_connection() -> Result<SqliteConnection, Error> {
-    dotenvy::dotenv().map_err(|_error| Error::new(ErrorKind::Other, "error"))?;
-    let url =
-        ::std::env::var("DATABASE_URL").map_err(|_error| Error::new(ErrorKind::Other, "error"))?;
+    fn drop_table(&mut self) -> Result<(), diesel::result::Error> {
+        diesel::sql_query("DROP TABLE IF EXISTS plannable_events")
+            .execute(&mut self.sql_connection)
+            .map(|size| ())
+    }
 
-    Ok(
-        SqliteConnection::establish(&url)
-            .map_err(|_error| Error::new(ErrorKind::Other, "error"))?,
-    )
+    fn create_table(&mut self) -> Result<(), diesel::result::Error> {
+        diesel::sql_query(
+            "CREATE TABLE plannable_events (
+             event_id BLOB PRIMARY KEY NOT NULL,
+             plannable_id TEXT NOT NULL,
+             sequence INTEGER NOT NULL,
+             body BLOB NOT NULL,
+             UNIQUE(plannable_id, sequence) 
+             ON CONFLICT ROLLBACK)",
+        )
+        .execute(&mut self.sql_connection)
+        .map(|size| ())
+    }
+}
+fn establish_connection(database_url: &str) -> Result<SqliteConnection, Error> {
+    dotenvy::dotenv().map_err(|_error| Error::new(ErrorKind::Other, "error"))?;
+    Ok(SqliteConnection::establish(&database_url)
+        .map_err(|_error| Error::new(ErrorKind::Other, "error"))?)
 }
 #[cfg(test)]
 mod tests {
-    use diesel::SqliteConnection;
 
     use crate::models::PlannableEventRow;
 
@@ -49,62 +61,18 @@ mod tests {
 
     #[test]
     fn initialize() {
-        let repository = PlannableEventsRepository::initialize();
+        let database_url = "/tmp/simple_plan_initialize.db";
+        let repository = PlannableEventsRepository::initialize(database_url);
         assert!(repository.is_ok());
     }
 
     #[test]
     fn save() {
-        let mut repository = PlannableEventsRepository::initialize().unwrap();
-        repository.reset().unwrap();
-        let plannables = vec![PlannableEventRow {
-            event_id: String::from("7fe8b15d-1a3e-461d-9057-99ef10459a0e")
-                .as_bytes()
-                .to_vec(),
-            plannable_id: String::from("1"),
-            sequence: 0,
-            body: String::from("").as_bytes().to_vec(),
-        }];
-        let result = repository.save(plannables);
-        println!("{:?}", result);
-        assert!(result.is_ok());
-    }
-    #[test]
-    fn save1() {
-        let mut repository = PlannableEventsRepository::initialize().unwrap();
-        repository.reset().unwrap();
-        let plannables = vec![PlannableEventRow {
-            event_id: String::from("7fe8b15d-1a3e-461d-9057-99ef10459a0e")
-                .as_bytes()
-                .to_vec(),
-            plannable_id: String::from("1"),
-            sequence: 0,
-            body: String::from("").as_bytes().to_vec(),
-        }];
-        let result = repository.save(plannables);
-        println!("{:?}", result);
-        assert!(result.is_ok());
-    }
-    #[test]
-    fn save2() {
-        let mut repository = PlannableEventsRepository::initialize().unwrap();
-        repository.reset().unwrap();
-        let plannables = vec![PlannableEventRow {
-            event_id: String::from("7fe8b15d-1a3e-461d-9057-99ef10459a0e")
-                .as_bytes()
-                .to_vec(),
-            plannable_id: String::from("1"),
-            sequence: 0,
-            body: String::from("").as_bytes().to_vec(),
-        }];
-        let result = repository.save(plannables);
-        println!("{:?}", result);
-        assert!(result.is_ok());
-    }
-    #[test]
-    fn save3() {
-        let mut repository = PlannableEventsRepository::initialize().unwrap();
-        repository.reset().unwrap();
+        let database_url = "/tmp/simple_plan_save.db";
+        let mut repository = PlannableEventsRepository::initialize(database_url).unwrap();
+        repository.drop_table().unwrap();
+        repository.create_table().unwrap();
+
         let plannables = vec![PlannableEventRow {
             event_id: String::from("7fe8b15d-1a3e-461d-9057-99ef10459a0e")
                 .as_bytes()
